fd4fb883e1e7f50fc98a65d8dbc1bf550344399a
Adnan-M123 <a_musanovic@outlook.com>
2025-03-26 19:43:46 +0100

    updated navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,19 @@ import { IoLogInOutline } from "react-icons/io5";
 
 
 export default function Navbar() {
-    return (
-      <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
-        {/* Left Icon */}
-        <div className="text-2xl font-bold">
-            <GrRestaurant />
-        </div>
-  
-        {/* Login Button */}
-        <button className="flex items-center gap-2 px-4 py-2 text-white bg-black rounded-lg hover:bg-gray-800">
-        <IoLogInOutline />
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-[#283618] shadow-md">
+      {/* Left Icon */}
+      <div className="text-2xl font-bold text-white flex flex-col items-center">
+        <GrRestaurant style={{ color: "white", fontSize: "24px" }} />
+        <span>DINERO</span>
+      </div>
+
+      {/* Login Button */}
+      <button className="flex items-center gap-2 px-4 py-2 text-[#283618] font-bold bg-white rounded-lg hover:bg-gray-800">
+        <IoLogInOutline style={{ fontSize: "24px"}}/>
         Log In
-        </button>
-      </nav>
-    );
-  }
+      </button>
+    </nav>
+  );
+}
