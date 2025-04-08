3c7a3133532a3ccbca37b0b58b16b884679abd18
Adnan-M123 <a_musanovic@outlook.com>
2025-04-08 11:15:57 +0200

    added routing back to main page from navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -17,7 +17,11 @@ export default function Navbar() {
 
   // Navigate to Restaurants page
   const handleRestaurantsClick = () => {
-    router.push('/restaurants');
+    router.push("/restaurants");
+  };
+
+  const handleLandPageClick = () => {
+    router.push("../");
   };
 
   // Manage body class for the blur effect when modal is open
@@ -38,17 +42,22 @@ export default function Navbar() {
       <nav className="flex items-center justify-between px-6 py-4 bg-[#283618] shadow-md flex-wrap">
         {/* Left Icon and Restaurants Button */}
         <div className="grid grid-cols-2 gap-4">
-          <div className="text-2xl font-bold text-white flex flex-col items-center space-y-2">
+          <button
+            onClick={handleLandPageClick}
+            className="text-2xl font-bold text-white flex flex-col items-center space-y-2 hover:text-gray-300 cursor-pointer hover:scale-105"
+          >
             <GrRestaurant style={{ color: "white", fontSize: "24px" }} />
             <span>DINERO</span>
-          </div>
+          </button>
 
           <div className="text-2xl font-bold text-white flex flex-col items-center space-y-2">
             <button
               onClick={handleRestaurantsClick}
-              className="flex flex-col items-center space-y-2 text-white hover:text-gray-300"
+              className="flex flex-col items-center space-y-2 text-white hover:text-gray-300 cursor-pointer hover:scale-105"
             >
-              <IoRestaurantOutline style={{ color: "white", fontSize: "24px" }} />
+              <IoRestaurantOutline
+                style={{ color: "white", fontSize: "24px" }}
+              />
               <span>RESTAURANTS</span>
             </button>
           </div>
@@ -56,7 +65,7 @@ export default function Navbar() {
 
         {/* Login Button */}
         <button
-          className="flex items-center gap-2 px-4 py-2 text-[#283618] font-bold bg-white rounded-lg hover:bg-gray-300 cursor-pointer"
+          className="flex items-center gap-2 px-4 py-2 text-[#283618] font-bold bg-white rounded-lg hover:bg-gray-300 cursor-pointer hover:scale-105"
           onClick={handleLoginClick}
         >
           <IoLogInOutline style={{ fontSize: "24px" }} />
